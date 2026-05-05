ad12ab0e1bde61be2c9dd743c1398dc5d7982b82
Zen Allaham <hxfhfucicic@gmail.com>
2026-05-05 19:37:50 +0300

    Update request.ts

--- frontend/app/api/user/request.ts
+++ frontend/app/api/user/request.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:7000";  
+export const BACKEND_URL = "https://codehive-socialmedia-application-2.onrender.com";  
 
 export const updateProfile = async (
   body: FormData | any,
